842f55a0893ca2be187f83a616f5c7f65ba2c165
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-14 01:56:06 +0530

    2nd Approach (Two Pointer Approach).

--- src/com/yogesh/arrays/MoveNeg.java
+++ src/com/yogesh/arrays/MoveNeg.java
@@ -22,11 +22,41 @@ public class MoveNeg {
             System.out.print(arr[i] + " ");
         }
 
+        // 2nd approach
+    //using Two Pointer Approach
+     public static void shiftall(int[] arr,int l,int r)
+     {
+        while(l <= r) {
+            if (arr[l] < 0 && arr[r]<0)
+                l++;
+         else if(arr[l] > 0 && arr[r] < 0)
+     {
+         int temp = arr[l];
+         arr[l] = arr[r];
+         arr[r] = temp;
+         l++;
+         r--;
+     }
+        else if (arr[l] > 0 && arr[r]>0)
+            r--;
+
+    else {
+        l++;
+        r--;
+    }
+        }
+     }
+
+    static void display(int [] arr,int r) {
+        for (int i =0; i<=r; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
     public static void main(String[] args) {
         int [] arr = {-1,2,3,4,-5,-6,-7,8,9};
-        int n = arr.length;
-        rearrange(arr,n);
-        printArray(arr,n);
+        int arr_size = arr.length;
+        shiftall(arr,0,arr_size-1);
+        display(arr,arr_size-1);
     }
 }
 
