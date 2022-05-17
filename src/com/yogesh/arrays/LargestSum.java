fa6a286055915720eec815895906abf62e0739f2
yogeshjangid <yogeshrj1433@gmail.com>
2022-05-18 01:10:55 +0530

    Minimum no. of Jumps to reach end of an array.

--- src/com/yogesh/arrays/LargestSum.java
+++ src/com/yogesh/arrays/LargestSum.java
@@ -28,23 +28,23 @@ public class LargestSum {
 
 //https://leetcode.com/problems/maximum-subarray/
 //Leetcode Submitted Question
-class Solution {
-
-
-    public int maxSubArray(int[] nums) {
-
-        int max_so_far=Integer.MIN_VALUE,max_ending_here=0;
-
-        for(int i=0;i<nums.length;i++){
-
-            max_ending_here+=nums[i];
-
-            if(max_ending_here>max_so_far)
-                max_so_far=max_ending_here;
-
-            if(max_ending_here<0)
-                max_ending_here=0;
-        }
-        return max_so_far;
-    }
-}
+//class Solution {
+//
+//
+//    public int maxSubArray(int[] nums) {
+//
+//        int max_so_far=Integer.MIN_VALUE,max_ending_here=0;
+//
+//        for(int i=0;i<nums.length;i++){
+//
+//            max_ending_here+=nums[i];
+//
+//            if(max_ending_here>max_so_far)
+//                max_so_far=max_ending_here;
+//
+//            if(max_ending_here<0)
+//                max_ending_here=0;
+//        }
+//        return max_so_far;
+//    }
+//}
